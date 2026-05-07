b56bd0a6256a3ce7c79ac5dca470bc1deb072857
dianee <dianeprincess817@gmail.com>
2026-05-07 15:00:35 +0800

    Delete UPMART directory

--- UPMART/dashboard/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-function addNotification($conn, $user_id, $message) {
-    $stmt = $conn->prepare("INSERT INTO notifications (user_id, message) VALUES (?, ?)");
-    $stmt->bind_param("is", $user_id, $message);
-    return $stmt->execute();
-}
-?>
